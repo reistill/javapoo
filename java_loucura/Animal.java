04460db000cb5747b52196b9738d8cde22dc2690
reistill <joelreistill@gmail.com>
2024-08-20 21:01:27 -0300

    "métodos do Animal"

--- java_loucura/Animal.java
+++ java_loucura/Animal.java
@@ -1,5 +1,25 @@
+//criação da classe(molde) abstrata Animal
 abstract class Animal {
-    
-    
+    private double tamanho;
+    private double peso;
+    private int idade;
+
+//criação dos métodos da classe animal
+    public void fazerbarulho() {
+        System.out.println("barulho animal");
+    }
+
+    public void comer() {
+        System.out.println("animal alimentando-se")
+    }
+
+    public void dormir() {
+        System.out.println("animal zzz");
+    }
+
+    public void vaguear() {
+        System.out.println("animal movimentando-se");
+    }
+
 }
 
